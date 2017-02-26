1ab35fd38867da9411f951bc22f95cad7f02cb0a
Jack <jack.degnen@gmail.com>
2017-02-26 00:37:11 +0000

    Major config rework

--- src/main/java/com/JackDeg/JacksFixes/handler/ConfigurationHandler.java
+++ src/main/java/com/JackDeg/JacksFixes/handler/ConfigurationHandler.java
@@ -1,7 +1,6 @@
 package com.JackDeg.JacksFixes.handler;
 
 import com.JackDeg.JacksFixes.Reference.Reference;
-import com.JackDeg.JacksFixes.util.LogHelper;
 import cpw.mods.fml.client.event.ConfigChangedEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.common.config.Configuration;
@@ -14,28 +13,31 @@ public class ConfigurationHandler
     public static Configuration configuration;
 
     public static void init(File configFile) {
-        configuration = new Configuration(configFile);
-
-        try {
-            configuration.load();
-
-            boolean skinning = configuration.get(Configuration.CATEGORY_GENERAL, "skinning", false, "Used to determine weather the skinning mechanic is used.").getBoolean(false);
-        } catch (Exception e) {
-
-        } finally {
-            if (configuration.hasChanged()) {
-                configuration.save();
-            }
+        if (configuration == null)
+        {
+            configuration = new Configuration(configFile);
         }
+
+
     }
 
-    /*@SubscribeEvent
+    @SubscribeEvent
     public void onConfigurationChangedEvent(ConfigChangedEvent.OnConfigChangedEvent event)
     {
         if(event.modID.equalsIgnoreCase(Reference.MOD_ID))
         {
-
+            configuration.load();
         }
-    }*/
+    }
+
+    public void loadConfiguration()
+    {
+       boolean skinning = configuration.get(Configuration.CATEGORY_GENERAL, "skinning", false, "Used to determine weather the skinning mechanic is used.").getBoolean(false);
+
+       if (configuration.hasChanged())
+       {
+           configuration.save();
+       }
+    }
 }
 
